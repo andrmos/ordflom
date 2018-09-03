7779386432d74c6167ea952ac80b4aa24c4719f3
andrmos <mossige.a@gmail.com>
2018-09-03 11:52:42 +0200

    Convert ChangeType to stateful component

--- src/containers/NySetning/ChangeType/index.jsx
+++ src/containers/NySetning/ChangeType/index.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./ChangeType.css";
 
-const ChangeType = () => (
-  <div>
-    <div>
-      <div className="button-line" />
-      <div className="button-line" />
-      <div className="button-line" />
-    </div>
-  </div>
-);
+class ChangeType extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div className="button-line" />
+          <div className="button-line" />
+          <div className="button-line" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ChangeType;
