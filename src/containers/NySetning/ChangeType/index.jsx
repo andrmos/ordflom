e7ca7043a020da833f05743cec6db52b58ff9e85
andrmos <mossige.a@gmail.com>
2018-09-03 12:05:18 +0200

    Display buttons for each type

--- src/containers/NySetning/ChangeType/index.jsx
+++ src/containers/NySetning/ChangeType/index.jsx
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { updateType } from "../../../reducers/view";
 
 import "./ChangeType.css";
+import Button from "../../../components/Button";
+
+// TODO: Move to utils
+const iconReference = {
+  generell: "comment-dots",
+  arkiv: "archive",
+  bistand: "hands-helping",
+  forsikring: "car-crash",
+  forsvar: "fighter-jet",
+  helse: "heart",
+  klima: "leaf",
+  bygg: "building",
+  mat: "utensils",
+  universitet: "graduation-cap"
+};
+const displayText = {
+  generell: "Generell svada",
+  arkiv: "Arkivsvada",
+  bistand: "Bistandssvada",
+  forsikring: "Forsikringssvada",
+  forsvar: "Forsvarssvada",
+  helse: "Helsesvada",
+  klima: "Klimasvada",
+  bygg: "Byggsvada",
+  mat: "Matsvada",
+  universitet: "Universitetssvada"
+};
 
 class ChangeType extends Component {
   state = {
@@ -8,7 +37,17 @@ class ChangeType extends Component {
   };
 
   render() {
-    const typeViews = <div>Types!</div>;
+    const { types, selectedType, updateType } = this.props;
+    const typeButtons = types.map(type => (
+      <Button
+        key={type}
+        text={displayText[type]}
+        selected={selectedType === type}
+        onClick={() => updateType(type)}
+        iconName={iconReference[type]}
+      />
+    ));
+
     return (
       <div>
         <div onClick={() => this.setState({ displayTypes: true })}>
@@ -17,10 +56,22 @@ class ChangeType extends Component {
           <div className="button-line" />
         </div>
 
-        {this.state.displayTypes ? typeViews : null}
+        {this.state.displayTypes ? typeButtons : null}
       </div>
     );
   }
 }
 
-export default ChangeType;
+const mapStateToProps = state => ({
+  types: Object.entries(state.svada).map(([type]) => type),
+  selectedType: state.view.selectedType
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateType: type => dispatch(updateType(type))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ChangeType);
