2a91a408e7e2abf5bd0de1fca594f4ff29f6650a
andrmos <mossige.a@gmail.com>
2018-09-03 11:58:54 +0200

    Display types on button click

--- src/containers/NySetning/ChangeType/index.jsx
+++ src/containers/NySetning/ChangeType/index.jsx
@@ -3,14 +3,21 @@ import React, { Component } from "react";
 import "./ChangeType.css";
 
 class ChangeType extends Component {
+  state = {
+    displayTypes: false
+  };
+
   render() {
+    const typeViews = <div>Types!</div>;
     return (
       <div>
-        <div>
+        <div onClick={() => this.setState({ displayTypes: true })}>
           <div className="button-line" />
           <div className="button-line" />
           <div className="button-line" />
         </div>
+
+        {this.state.displayTypes ? typeViews : null}
       </div>
     );
   }
