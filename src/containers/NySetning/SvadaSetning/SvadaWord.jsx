feed994af4f0d15db99079689212d5a1c2d7f521
andrmos <mossige.a@gmail.com>
2018-09-04 10:49:32 +0200

    Not assigned words have random length

--- src/containers/NySetning/SvadaSetning/SvadaWord.jsx
+++ src/containers/NySetning/SvadaSetning/SvadaWord.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { randomDecimal } from "../../../utils/utils";
+
 const SvadaWord = ({ word, onClick, selected }) => {
   // TODO: Refactor
   const isAssigned = word.length > 0;
@@ -11,7 +13,12 @@ const SvadaWord = ({ word, onClick, selected }) => {
       ? "svada-word not-assigned-word not-assigned-word-selected"
       : "svada-word not-assigned-word";
   return (
-    <div className={className} key={word} onClick={() => onClick()}>
+    <div
+      className={className}
+      style={{ width: isAssigned ? null : randomDecimal(5, 8) + "em" }}
+      key={word}
+      onClick={() => onClick()}
+    >
       {word}
     </div>
   );
